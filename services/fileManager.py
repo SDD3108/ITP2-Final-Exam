6618031c53e021b93d8b0c8af20dbefdc42ae8a4
DanialNurumbet <251250@astanait.edu.kz>
2026-05-21 10:51:03 +0500

    added function "wirte json"

--- services/fileManager.py
+++ services/fileManager.py
@@ -1,7 +1,15 @@
 import json
+from pathlib import Path
 from utils.decorators import safe_file
 
 @safe_file
 def read_json(path):
     with open(path,"r",encoding="utf-8") as file:
         return json.load(file)
+
+def write_json(path,data):
+    path=Path(path)
+    path.parent.mkdir(parents=True,exist_ok=True)
+
+    with open(path,"w",encoding="utf-8") as file:
+        json.dump(data,file,indent=2)
