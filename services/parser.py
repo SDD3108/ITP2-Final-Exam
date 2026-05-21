8900d880ab220837e4a9d819679d3ba4b703f826
DanialNurumbet <251250@astanait.edu.kz>
2026-05-21 11:03:10 +0500

    added function "parse_items"

--- services/parser.py
+++ services/parser.py
@@ -1,4 +1,6 @@
 from models.user import RegularUser,AdminUser
+from models.item import Item
+from utils.validators import is_valid_item_id
 
 def parse_users(data):
     users=[]
@@ -22,3 +24,20 @@ def parse_users(data):
         users.append(user)
 
     return users
+
+def parse_items(data):
+    items=[]
+
+    for(row) in data:
+        item_id=row["id"]
+
+        if(is_valid_item_id(item_id)):
+            items.append(
+                Item(
+                    item_id,
+                    row.get("title","no title"),
+                    row.get("genre","unknown")
+                )
+            )
+
+    return items
